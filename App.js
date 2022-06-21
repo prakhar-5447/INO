7dee33828aa11f935a9a9f3e6702b69696dd8b9d
pratham-0094 <sahupratham022003@gmail.com>
2022-06-21 17:04:03 +0530

    hide header from pages

--- App.js
+++ App.js
@@ -20,13 +20,13 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
         <Stack.Screen name="Login" component={Login}></Stack.Screen>
         <Stack.Screen name="Signup" component={Signup}></Stack.Screen>
-        <Stack.Screen
-          name="Profile"
-          options={{headerShown: false}}
-          component={Profile}></Stack.Screen>
+        <Stack.Screen name="Profile" component={Profile}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
